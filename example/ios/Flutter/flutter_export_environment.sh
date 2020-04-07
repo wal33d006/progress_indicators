95e926538abf2cc5f59f8b3b9f4e6e08ea5d89d5
wal33d006 <waleed_arshad006@hotmail.com>
2020-04-07 21:00:01 +0500

    Migrate example to android X. Update android dependencies.

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/waleedarshad/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/waleedarshad/Developer/FlutterApps/Plugins/progress_indicators/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/waleedarshad/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
